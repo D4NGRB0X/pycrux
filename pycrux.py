7e44e4fd7d32cea9f9e32e05a957d07555102102
Thomas <thomas.meche@gmail.com>
2020-08-22 13:38:04 -0600

    Removed ' ' from possible keys as it results in all Caps plain text "encryption"

--- pycrux.py
+++ pycrux.py
@@ -1,30 +1,30 @@
 import string
 import random
 
-xor_key = random.choice(string.printable)
+xor_key = random.choice([item for item in string.printable if item != ' '])
 print(xor_key)
 print(ord(xor_key))
 
+if ord(xor_key) != 32:
+    def encrypt(text, key):
+        encrypted = []
+        # to_encrypt = text.split()
+        for word in text:
+            encrypted_char = chr(ord(word) ^ ord(key))
+            # mutated_char = encrypted_char.decode('utf8')
+            encrypted.append(encrypted_char)
+        return ''.join(encrypted)
 
-def encrypt(text, key):
-    encrypted = []
-    # to_encrypt = text.split()
-    for word in text:
-        for letter in list(word):
-            encrypted_char = chr(ord(letter) ^ ord(key))
-            mutated_char = encrypted_char.encode('utf8').decode('utf8')
-            encrypted.append(mutated_char)
-    return ''.join(encrypted)
 
-
-def decrypt(text, key):
-    decrypted = []
-    for item in text:
-        mutated_char = item.encode('utf8').decode('utf8')
-        decrypt_char = chr(ord(mutated_char) ^ ord(key))
-        decrypted.append(decrypt_char)
-    return ''.join(decrypted)
-
+    def decrypt(text, key):
+        decrypted = []
+        for item in text:
+            # mutated_char = item.encode('utf8').decode('utf8')
+            decrypt_char = chr(ord(item) ^ ord(key))
+            decrypted.append(decrypt_char)
+        return ''.join(decrypted)
+else:
+    print("rerun")
 
 message = """this is the message
 with a new line"""
