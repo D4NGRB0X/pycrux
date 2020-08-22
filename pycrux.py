d187ad469ed1955f5c943d4e1765716a0e3aa82b
Thomas <thomas.meche@gmail.com>
2020-08-22 13:14:39 -0600

    Added decrypt method

--- pycrux.py
+++ pycrux.py
@@ -1,23 +1,34 @@
 import string
 import random
 
-xor_key = random.choice(string.ascii_letters)
+xor_key = random.choice(string.printable)
 print(xor_key)
 print(ord(xor_key))
 
 
 def encrypt(text, key):
     encrypted = []
-    if isinstance(text, str):
-        to_encrypt = text.split()
-        for word in to_encrypt:
-            for letter in list(word):
-                encrypted_char = chr(ord(letter) ^ ord(key))
-                encrypted.append(encrypted_char.encode('utf8').decode('utf8'))
+    # to_encrypt = text.split()
+    for word in text:
+        for letter in list(word):
+            encrypted_char = chr(ord(letter) ^ ord(key))
+            mutated_char = encrypted_char.encode('utf8').decode('utf8')
+            encrypted.append(mutated_char)
     return ''.join(encrypted)
-# def decrypt()
 
 
-message = "this is the message"
+def decrypt(text, key):
+    decrypted = []
+    for item in text:
+        mutated_char = item.encode('utf8').decode('utf8')
+        decrypt_char = chr(ord(mutated_char) ^ ord(key))
+        decrypted.append(decrypt_char)
+    return ''.join(decrypted)
 
-print(encrypt(message, xor_key))
+
+message = """this is the message
+with a new line"""
+
+encrypted_message = encrypt(message, xor_key)
+print(encrypted_message)
+print(decrypt(encrypted_message, xor_key))
